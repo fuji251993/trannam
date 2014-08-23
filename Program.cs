0badcc513a987359a16c8033e553de65d106bcb3
THKT53 Trần Xuân Nam <fujimaru251993@gmail.com>
2014-08-23 20:55:19 +0700

    first commit Signed-off-by: THKT53 Trần Xuân Nam <fujimaru251993@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        float a, b,s;
+            Console.Write("Nhap chieu dai:");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Nhap chieu rong:");
+            b = int.Parse(Console.ReadLine());
+            s = a * b;
+            Console.WriteLine("Dien tich la = {0}",s);
+            Console.ReadLine();
+        }
+    }
+}
